69e646718b25b29b532bac06356be92a25d6a09a
Gakuto1112 <gakuto131112@gmail.com>
2025-03-11 21:41:12 +0900

    各ファイルパスを修正

--- src/scripts/convert_to_base64.sh
+++ src/scripts/convert_to_base64.sh
@@ -1,5 +1,5 @@
-mkdir ~/base64_textures
+mkdir ./base64_textures
 
 for texture in $(cat ${COMPRESSED_TEXTURES} | jq .[]); do
-    base64 ./${TEXTURE_PATH}$(echo ${texture} | sed "s/\"//g") -w 0 > ~/base64_textures/$(echo ${texture} | sed -r 's/^"((.+)\/)?(.+\.png)"$/\3/').txt
+    base64 ./${TEXTURE_PATH}$(echo ${texture} | sed "s/\"//g") -w 0 > ./base64_textures/$(echo ${texture} | sed -r 's/^"((.+)\/)?(.+\.png)"$/\3/').txt
 done
--- src/scripts/get_compressed_textures.sh
+++ src/scripts/get_compressed_textures.sh
@@ -1,10 +1,14 @@
-git add -N --all
-git diff --name-only > ~/diff.log
+cd ./avatar
 
-if [ -s ~/diff.log ]; then
+git add -N --all
+git diff --name-only > ./diff.log
+
+cd ..
+
+if [ -s ./diff.log ]; then
     echo 'texture_compressed=true' >> $GITHUB_OUTPUT
 else
     echo 'texture_compressed=false' >> $GITHUB_OUTPUT
 fi
 
-echo "compressed_textures=$(cat ~/diff.log | awk -f ~/compressor/scripts/output_compressed_textures.awk)" >> $GITHUB_OUTPUT
+echo "compressed_textures=$(cat ./diff.log | awk -f ./compressor/scripts/output_compressed_textures.awk)" >> $GITHUB_OUTPUT
