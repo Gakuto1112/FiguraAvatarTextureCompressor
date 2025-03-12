a970cc96d6e144094a3a590ad453cb1ef5fcf39d
Gakuto1112 <gakuto131112@gmail.com>
2025-03-12 15:07:59 +0900

    エラーメッセージの出力を簡略化

--- src/scripts/check_paths.sh
+++ src/scripts/check_paths.sh
@@ -1,12 +1,12 @@
 no_directory_ditected=false
 
 if [ ! -d ${TEXTURE_PATH} ]; then
-    echo "[Error]: Cannot find directory \"$(echo ${TEXTURE_PATH} | sed -e 's/\.\/avatar\///g')\""
+    echo "::error::Cannot find directory \"$(echo ${TEXTURE_PATH} | sed -e 's/\.\/avatar\///g')\""
     no_directory_ditected=true
 fi
 
 if [ ! -d ${MODEL_PATH} ]; then
-     echo "[Error]: Cannot find directory \"$(echo ${MODEL_PATH} | sed -e 's/\.\/avatar\///g')\""
+     echo "::error::Cannot find directory \"$(echo ${MODEL_PATH} | sed -e 's/\.\/avatar\///g')\""
     no_directory_ditected=true
 fi
 
